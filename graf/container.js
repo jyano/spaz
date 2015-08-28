1b6d3143db1a07c27b9c7ffa5dcc8ea77d5dfa9e
yano <jason.yanofski@gmail.com>
2015-08-28 02:35:50 -0700

    progress

--- graf/container.js
+++ graf/container.js
@@ -289,3 +289,54 @@ cjs.iCt = function (ct) {
 //haha only a ct can impl addContainer( ha! //MOMENT OF BRILLIANCE
     return O(ct) && ct.addContainer
 }
+
+
+$PT()
+
+
+s.cannonBall=function(x,y){var s=this,h
+    h=s.h(x,y)
+    h.rf('a', 'z',18 ).dc(18).ef()
+    return h}
+s.basketBall=function(x,y){var s=this
+    return this.h(x,y).rf('w', 'o',18 ).dc(18).ef()
+}
+s.beachBall=function(x,y){var s=this
+    return s.h(x,y)
+        .rf('b','r',18).dc(18).ef()
+}
+s.snowBall=function(x,y){var s=this
+    return s.h(x,y).rf('a','w',18).dc(18).ef()
+}
+
+St=function(){z()
+    s = cjs.stg(1200,600).A()
+    h = $h(0,0).a2(s)
+    SL(h)
+}
+$St=function(){var st, g=G(arguments), cv
+    //get by canvas ID
+    cv = g.A? g.f[0]:
+        //if you pass it a canvas OR a $canvas object
+        O(g.f)? $(g.f)[0] :
+            //create a new canvas
+            $.c(g.f||'p',g.s||1200,g.t||600,g[3],g[4])[0]
+    st = new cjs.Stage( cv )
+    st.cv=   st.c=st.can= $(st.canvas)
+    st.cv0=  st.cv[0]
+    st.xc= st.cv0.getContext('2d')
+    if(g.p){st.t()}
+    return st.A()
+    //.t()
+}
+$.St=function(){z()
+    s = $St().t()
+    s.bm('me', function(bb){
+        b=bb.drag()
+    })
+    return s
+}
+$.dragStage = function (x, y) {
+    s = $St('v',  '+')
+    return $.dragFrame($(s.canvas))
+}
